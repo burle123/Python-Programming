1305add63d1c941b0a127c006daf4513022b31f3
burle123 <burleshantanu@gmail.com>
2025-11-01 14:16:53 +0530

    Print fn using formated string

---
+++ Chapter 2-PS/problem-7.py
@@ -0,0 +1,10 @@
+# WAP TO TAKE INPUT FROM USR AND PRINT THAT DATA USNG F STRING AND NORMAL STRING
+
+name=input("Enter your name : ")
+password=input("Enter your pass : ")
+email=input("Enter your email : ")
+gender=input("Enter your gender : ")
+mob=int(input("Enter you mob number : "))
+
+print(f"My name is {name} Password is {password} email = {email} gender = {gender} and phone number is {mob}")
+print(name,password,email,gender,mob)
