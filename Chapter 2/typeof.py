732d0f60906b939189752a4bb1ddf1591acd6b0e
burle123 <burleshantanu@gmail.com>
2025-11-01 14:22:30 +0530

    type conversion

--- Chapter 2/typeof.py
+++ Chapter 2/typeof.py
@@ -1,5 +1,10 @@
-a = "31.2"
-b = float(a) # a but the type should be float
-t = type(b) 
+# a = "31.2"
+# b = float(a) # a but the type should be float
+# t = type(b) 
 
-print(t)
+# print(t)
+
+a=10
+b=int("32")
+sum=a+b
+print(sum)
