935a54ad9c4f9f821bf919192bef91160dd0b20a
burle123 <burleshantanu@gmail.com>
2025-12-30 14:12:58 +0530

    Constructor

---
+++ Chapter 10-PS/problem9.py
@@ -0,0 +1,15 @@
+#Create student class that takesname and marks of three sub as argument in constructor.
+#Then create a method to print avg marks 
+
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def get_avg(self):
+        sum=0
+        for val in self.marks:
+            sum+=val
+        print("Hello, ",self.name,"Your avg score is",sum/3)    
+
+s1=Student("Shantanu",[78,97,65])
+s1.get_avg()        
