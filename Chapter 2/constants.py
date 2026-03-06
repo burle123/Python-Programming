f2b34a9d8695db36b8fcb57bb851e0ab1742afc8
burle123 <burleshantanu@gmail.com>
2026-03-06 13:47:03 +0530

    Constants

---
+++ Chapter 2/constants.py
@@ -0,0 +1,10 @@
+BATCH_SIZE = 35
+BATCH_NAME = "PYTHON B1"
+
+present_count = 25
+absent_count = BATCH_SIZE - present_count
+print("Today's Details:")
+print("Batch Name:",BATCH_NAME)
+print("Total Students:", BATCH_SIZE)
+print("Total present students:",present_count)
+print("Total absent student:",absent_count)
